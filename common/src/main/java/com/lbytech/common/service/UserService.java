7e90b9963c7dfe4c35f1b66d8d917c37fa3d1e7e
lby-magicbook <1508201988@qq.com>
2025-06-10 15:13:41 +0800

    提供一个获取用户的方法

--- common/src/main/java/com/lbytech/common/service/UserService.java
+++ common/src/main/java/com/lbytech/common/service/UserService.java
@@ -1,4 +1,13 @@
 package com.lbytech.common.service;
 
+import com.lbytech.common.model.User;
+
 public interface UserService {
+
+    /**
+     * 获取用户
+     * @param user
+     * @return
+     */
+    User getUser(User user);
 }
