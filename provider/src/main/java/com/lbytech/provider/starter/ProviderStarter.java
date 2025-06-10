03fab00e67cd54caa8e3e4b29b488f441e8b41a7
lby_rog <1508201988@qq.com>
2025-06-10 21:05:06 +0800

    验证web服务器能够正常启动

--- provider/src/main/java/com/lbytech/provider/starter/ProviderStarter.java
+++ provider/src/main/java/com/lbytech/provider/starter/ProviderStarter.java
@@ -1,8 +1,13 @@
 package com.lbytech.provider.starter;
 
+import com.lbytech.rpc.server.HttpServer;
+import com.lbytech.rpc.server.impl.VertxHttpServer;
+
 public class ProviderStarter {
 
     public static void main(String[] args) {
-
+        //启动web服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
