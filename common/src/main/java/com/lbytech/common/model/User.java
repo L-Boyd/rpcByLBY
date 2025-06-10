32367f71a87cbcaf03d1065286f3677c922bbcfa
lby-magicbook <1508201988@qq.com>
2025-06-10 15:15:53 +0800

    实现序列化接口

--- common/src/main/java/com/lbytech/common/model/User.java
+++ common/src/main/java/com/lbytech/common/model/User.java
@@ -1,9 +1,10 @@
 package com.lbytech.common.model;
 
 import lombok.Data;
+import java.io.Serializable;
 
 @Data
-public class User {
+public class User implements Serializable {
 
     private String name;
 
